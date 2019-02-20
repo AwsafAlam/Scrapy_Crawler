b126d4f6dabe4a6c5306e673378efd41dbeab237
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2019-02-20 17:18:01 +0600

    Addded multiple crawlers with recursive crawling

--- crwl/crwl/spiders/myspider.py
+++ crwl/crwl/spiders/myspider.py
@@ -2,18 +2,39 @@ import scrapy
 
 class WordScraper(scrapy.Spider):
 
-    name = "quotes"
+    # name = "quotes"
 
-    def start_requests(self):
-        urls = [
-                'http://quotes.toscrape.com/page/1/',
-                'http://quotes.toscrape.com/page/2/']
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    # def start_requests(self):
+    #     urls = [
+    #             'http://quotes.toscrape.com/page/1/',
+    #             'http://quotes.toscrape.com/page/2/']
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+
+    # def parse(self, response):
+    #     page = response.url.split("/")[-2]
+    #     filename = 'quotes-%s.html' % page
+    #     with open(filename, 'wb') as f:
+    #         f.write(response.body)
+    #     self.log('Saved file %s' % filename)
+
+    name = "quotes"
+    
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+        'http://quotes.toscrape.com/page/3/',
+        'http://quotes.toscrape.com/page/4/',
+        'http://quotes.toscrape.com/page/5/',
+        'http://quotes.toscrape.com/page/6/',
+        'http://quotes.toscrape.com/page/7/',
+        'http://quotes.toscrape.com/page/8/',
+    ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
